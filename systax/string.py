75bfb2392035b57ef761f457b828ca85ae9117cc
707BIGBOSS\user <2001mist@naver.com>
2022-04-24 23:38:42 +0900

    python 숫자, 문자 연습 홈페이지 CGI로 구현

---
+++ systax/string.py
@@ -0,0 +1,19 @@
+from msvcrt import LK_LOCK
+from re import L
+
+
+print('Hello world')
+print("hello world")
+print("hell'o' world")
+#escape 
+print("hell'o' \"w\"orld")
+#newline
+print('h\ne\nl\nl\no')
+#docstring
+print('''
+h
+e
+l
+l
+o
+''')
