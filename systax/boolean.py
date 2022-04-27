e3113f8bfd31e52c977621c3e21a974fd3b06bce
707BIGBOSS\user <2001mist@naver.com>
2022-04-27 22:30:18 +0900

    문법-boolean

---
+++ systax/boolean.py
@@ -0,0 +1,20 @@
+#Number
+print(1) #Integer
+#String 
+print('Hello World')
+#Boolean
+print(True) #참
+print(False) #거짓
+
+#Expression
+print(1+1)
+print('Hello ' + 'World')
+#Comparison operator
+print(1==1) #True
+print(1<2) #True
+print(2<1) #False
+#Membership operator
+print('world' in 'Hello World') #True
+
+import os.path
+print(os.path.exists('boolean2.py'))
