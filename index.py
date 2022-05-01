4fd467e94553631f001d7796be982c4d45199b12
707BIGBOSS\user <2001mist@naver.com>
2022-05-01 13:12:32 +0900

    파일제어와 본문기능 구현 description 이라는 것이 id 값이 있는 경우에는 처리되지 않기 때문에 오류가 생긴다.
    현재 main 만 보이고 link 누르면 오류

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ if 'id' in form:
   pageId = form["id"].value
 else:
   pageId = 'welcome'
+  description = 'hello, web'  # 이라는 것이 id 값이 있는 경우에는 처리되지 않기 때문에 오류가 생긴다.
 print('''<!doctype html>
 <html>
 <head>
@@ -21,8 +22,7 @@ print('''<!doctype html>
     <li><a href="index.py?id=JavaScript">JavaScript</a></li>
   </ol>
   <h2>{title}</h2>
-  <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-  </p>
+  <p>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId))
+'''.format(title=pageId, desc=description))
